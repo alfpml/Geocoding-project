80e035b365a5b8f46580e1f9340ff5ae812ec41c
alfpml <alfpml@gmail.com>
2019-11-20 01:38:57 +0100

    commit

--- src/clean_mongo.py
+++ src/clean_mongo.py
@@ -53,6 +53,7 @@ name=[]
 category=[]
 description=[]
 city=[]
+country=[]
 latitude=[]
 longitude=[]
 year_founded=[]
@@ -66,24 +67,29 @@ for c in range(len(companies2)):
         category.append(companies2[c].get('category_code',0))
         description.append(companies2[c].get('description',0))
         city.append(companies2[c].get('offices',0)[office].get('city'))
+        country.append(companies2[c].get('offices',0)[office].get('country_code'))
         latitude.append(companies2[c].get('offices',0)[office].get('latitude'))
         longitude.append(companies2[c].get('offices',0)[office].get('longitude'))
         year_founded.append(companies2[c].get('founded_year',0))
         employees.append(companies2[c].get('number_of_employees',0))
         total_money_raised.append(companies2[c].get('total_money_raised',0))
 
-filtered={"idt":idt,"name":name, "category":category,"description":description,"city":city,"latitude":latitude,"longitude":longitude,"year_founded":year_founded,"employees":employees,"total_money_raised":total_money_raised}
+filtered={"idt":idt,"name":name, "category":category,"description":description,"city":city,"country":country,"latitude":latitude,"longitude":longitude,"year_founded":year_founded,"employees":employees,"total_money_raised":total_money_raised}
 filtered_df=pd.DataFrame(filtered)
-filtered=filtered_df.dropna(subset = ['city','latitude','longitude'])
+filtered=filtered_df.dropna(subset = ['latitude','longitude'])
 filtered=filtered_df.dropna(subset = ['city'])
+filtered=filtered[filtered['city'].notnull()]
+filtered=filtered[filtered['latitude'].notnull()]
+filtered=filtered[filtered['longitude'].notnull()]
+
+
 
 city_counts=filtered['city'].value_counts()
-print(city_counts.head(20))
+
+print(city_counts.head(30))
 
 
 print(len(filtered))
 print(filtered.head(10))
 filtered.to_csv("./output/filt_comp_coords.csv")
 
-
-##companies having raised over a million dollars
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import pandas as pd
+import geopy.distance
 
 def connectCollection(database, collection):
     client = MongoClient()
@@ -7,70 +8,5 @@ def connectCollection(database, collection):
     coll = db[collection]
     return db, coll
 
-db, coll = connectCollection('companies_cb','companies_cb')
-
-##find companies with more than 50 employees or having funding rounds
-
-companies = coll.find(
-{
-    '$or': [
-        {
-            '$and': [
-                {
-                    'number_of_employees': None
-                },{
-                    'funding_rounds.raised_amount': {
-                        '$gt': 250000
-                    }
-                },{
-                    'founded_year':{'$gte': 2003}
-                }
-            ]
-        }, 
-        {
-            '$and': [
-        
-        
-        {
-            'number_of_employees': {
-                '$gt': 50
-            }}
-        ,{
-            'founded_year':{'$gte': 2003}
-            }
-        ]
-        }
-    ]
-}
-)
-
-companies2=list(companies)
-print(len(companies2))
-
-
-idt=[]
-name=[]
-category=[]
-description=[]
-latitude=[]
-longitude=[]
-year_founded=[]
-employees=[]
-
-for c in range(len(companies2)):
-    for office in range(len(companies2[c].get('offices',0))):
-        idt.append(companies2[c].get('_id',0))
-        name.append(companies2[c].get('name',0))
-        category.append(companies2[c].get('category_code',0))
-        description.append(companies2[c].get('description',0))
-        latitude.append(companies2[c].get('offices',0)[office].get('latitude'))
-        longitude.append(companies2[c].get('offices',0)[office].get('longitude'))
-        year_founded.append(companies2[c].get('founded_year',0))
-        employees.append(companies2[c].get('number_of_employees',0))
-
-filtered={"idt":idt,"name":name, "category":category,"description":description,"latitude":latitude,"longitude":longitude,"year_founded":year_founded,"employees":employees}
-filtered_df=pd.DataFrame(filtered)
-filtered=filtered_df.dropna(subset = ['latitude','longitude'])
-print(len(filtered))
-print(filtered.head(10))
-filtered.to_csv("./output/filt_comp_coords.csv")
+def distance(coord_1,coord_2):
+return geopy.distance.vincenty(coords_1, coords_2).km
